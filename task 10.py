a49c098afb87157bedd0da508956689af6032fd7
shaunnoeljose <45419864+shaunnoeljose@users.noreply.github.com>
2019-09-20 14:17:18 +0530

    Delete task 10.py

--- task 10.py
+++
@@ -1,8 +0,0 @@
-class rectangle():
-  def __init__(self,l,b):
-    self.length=l
-    self.breadth=b
-  def area(self):
-    return self.length*self.breadth
-Myrectangle=rectangle(5,8)
-print(Myrectangle.area())
